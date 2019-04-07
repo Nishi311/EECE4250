db643941a0c8013c77939298f46b865588c009de
Nick Montaquila <nmontaquila3@gmail.com>
2019-04-07 14:09:29 -0400

    Commenting out the store new quiz controller call for now

--- Flask/app.py
+++ Flask/app.py
@@ -23,6 +23,7 @@ app.config['MYSQL_DATABASE_DB'] = "YCS"
 app.config['MYSQL_DATABASE_HOST'] = "yuppie-city-simulator-db.cohu57vlr7rd.us-east-2.rds.amazonaws.com"
 mysql.init_app(app)
 
+
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -31,8 +32,9 @@ def login_required(f):
         else:
             flash('You need to login first!')
             redirect(url_for('home'))
-    
+
     return wrap
+
 
 test_db = defaultdict(lambda: '')
 test_db['user_id'] = '10000'
@@ -77,7 +79,7 @@ def login():
         else:
             error = 'Invalid credentials! Please try again.'
             flash('Invalid credentials')
-            
+
     return render_template('index.html', error=error)
 
 
@@ -89,7 +91,7 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/create', methods = ['GET', 'POST'])
+@app.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
         test_db[request.form['username']] = request.form['password']
@@ -97,32 +99,34 @@ def create():
 
     return render_template('index.html')
 
+
 @app.route('/quizResults', methods=['GET', 'POST'])
 def quiz_results():
     if request.method == 'POST':
         weight_list = []
-        attribute_list = "walkability, " + "bikeability, " + "transit, "+ "traffic, " + "metro_pop, " + "pop_density, " + "prop_crime, " + "violent_crime, " + "air_pollution, " +  "sunshine"
+        attribute_list = "walkability, " + "bikeability, " + "transit, " + "traffic, " + "metro_pop, " + \
+            "pop_density, " + "prop_crime, " + \
+            "violent_crime, " + "air_pollution, " + "sunshine"
         attribute_list = attribute_list.split(", ")
         print(attribute_list)
         for factor in attribute_list:
             weight_list.append(request.form[factor])
-        # weight_list = request.json.split(", ")
         print(weight_list)
 
         if len(attribute_list) == len(weight_list):
             combined_dict = {}
 
             for index in range(len(attribute_list)):
-                combined_dict[attribute_list[index]] = float(weight_list[index])
+                combined_dict[attribute_list[index]
+                              ] = float(weight_list[index])
 
             raw_quiz_results = QuizResults(combined_dict)
 
             algo_runner = AlgorithmRunner()
             processed_quiz_results = algo_runner.run_module(raw_quiz_results)
             # TODO: Ask nick about how to get the actual user ID
-            controller.store_new_quiz(99999, processed_quiz_results)
+            # controller.store_new_quiz(99999, processed_quiz_results)
 
-            # print(processed_quiz_results.print_cities())
             city_scores_dict = defaultdict()
             city_names = []
             for city_tuple in processed_quiz_results.return_city_scores():
@@ -133,11 +137,11 @@ def quiz_results():
 
             conn = mysql.connect()
             cursor = conn.cursor(pymysql.cursors.DictCursor)
-            cursor.execute("SELECT * FROM city_index WHERE city_name IN ('{0}','{1}','{2}','{3}','{4}')".format(city_names[0], city_names[1], city_names[2], city_names[3], city_names[4]))
+            cursor.execute("SELECT * FROM city_index WHERE city_name IN ('{0}','{1}','{2}','{3}','{4}')".format(
+                city_names[0], city_names[1], city_names[2], city_names[3], city_names[4]))
             rows = cursor.fetchall()
             print(rows)
 
-            # return jsonify(processed_quiz_results.print_cities())
         return render_template('view_results.html', scores=city_scores_dict, data=rows)
 
 
